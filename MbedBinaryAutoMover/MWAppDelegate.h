bc64552f59c15918d3a40c4b7cb615aaece23f08
edisonthk-mac <edisonthk@gmail.com>
2014-06-14 16:22:04 +0900

    Initial Commit

---
+++ MbedBinaryAutoMover/MWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MWAppDelegate.h
+//  MbedBinaryAutoMover
+//
+//  Created by Edisonthk on 2014/06/14.
+//  Copyright (c) 2014年 Maizework. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MWAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MbedBinaryAutoMover/MWAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MWAppDelegate.m
+//  MbedBinaryAutoMover
+//
+//  Created by Edisonthk on 2014/06/14.
+//  Copyright (c) 2014年 Maizework. All rights reserved.
+//
+
+#import "MWAppDelegate.h"
+
+@implementation MWAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
